b5e24053f62348e7bdcf2e2fbaa5088ed0bb7d81
helloxpluto <deplanetart@gmail.com>
2020-12-18 09:09:33 -0800

    mm

--- accounts/views.py
+++ accounts/views.py
@@ -11,7 +11,7 @@ def sign_up(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('gift_detail')
+            return redirect('sample_list')
     else:
         form = UserCreationForm()
     return render(request, 'accounts/signup.html', {'form': form})
